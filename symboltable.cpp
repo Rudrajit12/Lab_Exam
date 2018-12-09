dd4c01e531af91dcd589d6e1a7af479d6ad531bd
Rudrajit Bhattacharyya <38225464+Rudrajit12@users.noreply.github.com>
2018-12-09 16:50:22 +0530

    Update symboltable.cpp

--- symboltable.cpp
+++ symboltable.cpp
@@ -8,9 +8,9 @@ using namespace std;
 
 int isKeyword(char buffer[])
 {
-	char keywords[32][10] = {"auto","break","case","char","const","continue","default","do","double","else","enum","extern","float"
-							,"for","goto","if","int","long","register","return","short","signed","sizeof","static","struct","switch"
-							,"typedef","union","unsigned","void","volatile","while"};
+	char keywords[32][10] = {"auto","break","case","char","const","continue","default","do","double","else","enum","extern",
+		"float","for","goto","if","int","long","register","return","short","signed","sizeof","static","struct","switch"
+		,"typedef","union","unsigned","void","volatile","while"};
 	int i, flag = 0;
 	for(i=0;i<32;i++)
 	{
